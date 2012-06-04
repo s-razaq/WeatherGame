f046e3b04c9c32a7f60ebe37f299c56facae44ba
slaughter89 <slaughter89@a60d8476-77d0-4065-9a47-c303846ceabc>
2012-06-04 13:30:35 +0000

    git-svn-id: https://svn.code.sf.net/p/androdhbw/code/trunk/Gruppe5/BestWeatherGameEver/BestWeatherGameEver@98 a60d8476-77d0-4065-9a47-c303846ceabc

---
+++ src/com/sample/Gamification.java
@@ -0,0 +1,52 @@
+
+
+public class Gamification {
+	private static Gamification instance = null;
+	private int score;
+	private String[] levelDescription = {"Total Beginner", "Weather Noob", "Estimator", "Weather Expert", "Master Forecaster", "Weather Wizard"};
+	private int[] pointsNeededForLevel = {0,40,100,200,350,555};
+
+	 public static Gamification getInstance() {
+	        if (instance == null) {
+	            instance = new Gamification();
+	        }
+	        return instance;
+	    }
+	
+	private Gamification(){
+		//score = aus Settings laden
+		
+	}
+	
+	
+	public int newScoreForResult(int deviation){
+		int level = this.calculateLevel();
+		score += 12-level-deviation;
+		System.out.println("new score:" + score);		
+		return score;
+	}
+	
+	public int getLevel(){
+		return this.calculateLevel();
+	}
+	
+	public int getScore(){
+		return score;
+	}
+	
+	private int calculateLevel(){
+		int level = 0;
+		for(int i = 1;i<=levelDescription.length;i++){
+			if (score < pointsNeededForLevel[i-1]){
+				level = i-1;
+				break;
+			} else {
+				level = i;
+			}
+		}
+		System.out.println("current level: " + level + " " + levelDescription[level-1]);
+		return level;
+	}
+	
+	
+}
---
+++ src/com/sample/TestGamification.java
@@ -0,0 +1,20 @@
+
+
+public class TestGamification {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gamification gamification = Gamification.getInstance();
+		for(int i = 0; i<1000; i++){
+			gamification.newScoreForResult(5);
+		}
+		
+		
+
+	}
+
+}
