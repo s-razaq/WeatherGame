ce93fc367109e3cfbe3985bcd5d4bc1978738f82
srazaq <srazaq@a60d8476-77d0-4065-9a47-c303846ceabc>
2012-06-05 17:22:17 +0000

    Implemented Weather Service. How-to: 1st: Create new instance => WeatherService ws = WeatherService.getInstance(); 2nd: Call getTemperature function => ws.getTemperature("Heilbronn");
    git-svn-id: https://svn.code.sf.net/p/androdhbw/code/trunk/Gruppe5/BestWeatherGameEver/BestWeatherGameEver@104 a60d8476-77d0-4065-9a47-c303846ceabc

---
+++ src/com/sample/WeatherService.java
@@ -0,0 +1,104 @@
+package com.sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class WeatherService {
+
+	static HttpClient client;
+	private HttpGet request;
+	private HttpResponse response;
+
+	private final String BASE_URL = "http://free.worldweatheronline.com/feed/weather.ashx";
+	private final String KEY = "adaf549abd120300122505";
+	private final String FORMAT = "json";
+	private String requestValue = "Karlsruhe";
+	private String urlString;
+
+	private StringBuilder jsonResponse;
+
+	/* Klassische Implementierung des Singleton-Patterns */
+	private static WeatherService instance = null;
+
+	/**
+	 * Default-Konstruktor, der nicht außerhalb dieser Klasse aufgerufen werden
+	 * kann
+	 */
+	private WeatherService() {
+	}
+
+	/**
+	 * Statische Methode, liefert die einzige Instanz dieser Klasse zurück
+	 */
+	public static WeatherService getInstance() {
+		if (instance == null) {
+			instance = new WeatherService();
+			client = new DefaultHttpClient();
+		}
+		return instance;
+	}
+
+	public double getTemperature(String city) {
+		requestValue = city;
+		postRequest();
+		return parseJSON(jsonResponse.toString());
+	}
+
+	private void createUrlString() {
+		urlString = BASE_URL + "?key=" + KEY + "&q=" + requestValue
+				+ "&format=" + FORMAT;
+	}
+
+	private void postRequest() {
+		try {
+			createUrlString();
+			System.out.println(urlString);
+			request = new HttpGet(urlString);
+			response = client.execute(request);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				jsonResponse = new StringBuilder();
+				BufferedReader br = new BufferedReader(new InputStreamReader(
+						response.getEntity().getContent()));
+				String line;
+				while ((line = br.readLine()) != null) {
+					jsonResponse.append(line);
+				}
+			} else {
+				Log.e("BestWeatherGame", "Failed to download file");
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private double parseJSON(String value) {
+		double d = 404;
+		try {
+			JSONObject jsonObj = new JSONObject(value).getJSONObject("data")
+					.getJSONArray("current_condition").getJSONObject(0);
+			d = Double.parseDouble(jsonObj.getString("temp_C"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return d;
+	}
+
+}
