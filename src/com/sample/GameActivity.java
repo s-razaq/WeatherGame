39ade03a4cca068fb14849da77503b2990c04622
viggi1987 <viggi1987@a60d8476-77d0-4065-9a47-c303846ceabc>
2012-06-06 11:32:23 +0000

    git-svn-id: https://svn.code.sf.net/p/androdhbw/code/trunk/Gruppe5/BestWeatherGameEver/BestWeatherGameEver@110 a60d8476-77d0-4065-9a47-c303846ceabc

--- src/com/sample/GameActivity.java
+++ src/com/sample/GameActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class GameActivity extends Activity {
 	
-	   public void onCreate(Bundle savedInstanceState) {
+	static String[] wheelMenu = new String[100];
+	private boolean wheelScrolled = false;
+	
+	public void onCreate (Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        
 	        final Intent intent = this.getIntent();
@@ -21,11 +25,13 @@ public class GameActivity extends Activity {
 	        TextView stadt = (TextView) findViewById(R.id.stadt);
 	        Button enterButton =(Button) findViewById(R.id.enterButton);
 	   
-	   
+	        for (int i = 0; i <= 100; i++)
+	    		wheelMenu[i] = "" + (-50 + i);
+	    	
 	        //Random City
 	        RandCity randCity = RandCity.getInstance(this.getApplicationContext());
 	        String city = randCity.getCity(difficulty);
-	        stadt.setText(city);		        
+	        stadt.setText(city);	
 	   
 	   }
 }
