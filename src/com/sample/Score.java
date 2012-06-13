da138fb055a095aa7d38a5dcbe585b0ca713abe0
simonweb <simonweb@a60d8476-77d0-4065-9a47-c303846ceabc>
2012-06-13 09:11:00 +0000

    git-svn-id: https://svn.code.sf.net/p/androdhbw/code/trunk/Gruppe5/BestWeatherGameEver/BestWeatherGameEver@153 a60d8476-77d0-4065-9a47-c303846ceabc

--- src/com/sample/Score.java
+++ src/com/sample/Score.java
@@ -8,26 +8,52 @@ import java.io.IOException;
 import android.os.Environment;
 
 public class Score {
-	
-	public Score(){
-		String filename = "score.scr";
-		Integer scor;
+	int score = 0;
+	String filename = "score";
+	public Score(int x){
+		score = x;
 		File file = new File(Environment.getExternalStorageDirectory(), filename);
 		if (!file.exists()){
+		this.save();
+		}else{
+		try {
+		    InputStream instream = openFileInput(this.filename);
+		    if (instream) {
+		      // prepare the file for reading
+		      InputStreamReader inputreader = new InputStreamReader(instream);
+		      BufferedReader buffreader = new BufferedReader(inputreader);
+		      String line = buffreader.readLine();
+		      score = Integer.parseInt(line);
+		      }
+		    instream.close();
+		 
+		} catch (Exception e) {
+		    // handle exception
+		}
+		
+		}
+	}
+	public int getScore(){
+		return score;
+	
+	}
+	public void setScore(int x){
+		score = x;
+		this.save()
+	}
+	
+	private void save(){
+		File file = new File(Environment.getExternalStorageDirectory(), this.filename);
 		FileOutputStream fos;
 		try {
 		    fos = new FileOutputStream(file);
-		    fos.write(0);
+		    fos.write(score.toString());
 		    fos.flush();
 		    fos.close();
-		} catch (FileNotFoundException e) {
-		    // handle exception
-		} catch (IOException e) {
+		} catch (Exception e) {
 		    // handle exception
 		}
-	}else{
+	
 		
-	}
 		
-	}
 }
