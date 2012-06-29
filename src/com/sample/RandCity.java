88a962730c18c9138e01e8579dc6b73c1a5353ab
jokosarju <jokosarju@a60d8476-77d0-4065-9a47-c303846ceabc>
2012-06-29 11:51:58 +0000

    Comments in RandCity
    git-svn-id: https://svn.code.sf.net/p/androdhbw/code/trunk/Gruppe5/BestWeatherGameEver/BestWeatherGameEver@286 a60d8476-77d0-4065-9a47-c303846ceabc

--- src/com/sample/RandCity.java
+++ src/com/sample/RandCity.java
@@ -10,39 +10,51 @@ import android.content.res.AssetManager;
 import android.os.AsyncTask;
 
 
-public class RandCity extends AsyncTask {
-	
+public class RandCity {
+
 	private static AssetManager assetManager;
 	private static ArrayList<String> easyList = new ArrayList<String>(), mediumList= new ArrayList<String>(), hardList= new ArrayList<String>();
-	
+
+	/*
+	 * Singleton Pattern
+	 */
 	private static RandCity instance = null;
 	public static RandCity getInstance(Context iAppContext) {
-        if (instance == null) {
-            instance = new RandCity(iAppContext);
-        }
-        return instance;
-    }
+		if (instance == null) {
+			instance = new RandCity(iAppContext);
+		}
+		return instance;
+	}
 	private RandCity(Context iAppContext) {
+		// Assign App Context to local static variable
 		assetManager = iAppContext.getAssets();
 		
 	}
+	
+	/*
+	 *  Initially Fills ArrayLists
+	 */
 	public void loadCSV() {
 		fillArray("easy");
 		fillArray("medium");
 		fillArray("hard");
 	}
 
+	/*
+	 * Access the ArrayList from other classes
+	 */
 	public String getCity(String iDifficulty) {
-		
+
 		String city = null;
 		int rand = 0;
 		if(iDifficulty.equals("easy")) {
-			//for(int i = 0; i < easyList.size();i++) System.out.println(easyList.get(i));
+			// Generates random access key
 			rand = (int) (Math.random() * (easyList.size()-1));
 			city = easyList.get(rand);
+			// Assures that random number is removed from ArrayList
 			easyList.remove(rand);
 			easyList.remove(city);
-			//System.out.println(easyList.size());
+			// Automatically refills ArrayList in case it is empty
 			if(easyList.size() == 0) {
 				fillArray(iDifficulty);
 			}
@@ -52,80 +64,79 @@ public class RandCity extends AsyncTask {
 			city = mediumList.get(rand);
 			mediumList.remove(rand);
 			mediumList.remove(city);
-			if(mediumList.size() == 0) fillArray(iDifficulty);
+			if(mediumList.size() == 0) {
+				fillArray(iDifficulty);
+			}	
 		}
 		else {
 			rand = (int)(Math.random() * (hardList.size()-1));
 			city = hardList.get(rand);
 			hardList.remove(rand);
 			hardList.remove(city);
-			if(hardList.size() == 0) fillArray(iDifficulty);
+			if(hardList.size() == 0) {
+				fillArray(iDifficulty);
+			}
 		}
 		System.out.println(city);
 		return city;
 	}
-	
-	/*public String getLatLong(String iDifficulty) {
-		if(iDifficulty == "easy" || iDifficulty == "medium") return getCity(iDifficulty);
-		
-		
-		
-		return "";
-	}*/
-	
+
+	/*
+	 * Read CSV Tables
+	 */
 	private void fillArray(String iDifficulty){
-		
+
+		// Initiate Streams
 		InputStream stream = null;
 		BufferedInputStream readbuffer = null; 
-        byte[] buffer = new byte[100000];
-        int bytesRead; 
-        try {
-            stream = assetManager.open(iDifficulty+".csv");
-            
-            readbuffer = new BufferedInputStream(stream);
-            bytesRead = 0;
-            String[] splitArrayLine = null;
-            String[] splitArrayTab = null;
-            
-            while ((bytesRead = readbuffer.read(buffer)) != -1) {
-              String rawCity = new String(buffer, 0, bytesRead);
-              System.out.print(rawCity);
-              splitArrayLine = rawCity.split("\n");
-            }
-            
-            System.out.println(""+splitArrayLine.length);
-            
-            for(int i = 0; i < splitArrayLine.length; i++) {
-            	splitArrayTab = splitArrayLine[i].split("\t");
+		byte[] buffer = new byte[100000];
+		int bytesRead; 
+		try {
+			// Open File
+			stream = assetManager.open(iDifficulty+".csv");
 
-            	if(iDifficulty.equals("easy")) easyList.add(splitArrayTab[1].toString()+";"+splitArrayTab[2].toString());
-            	if(iDifficulty.equals("medium")) mediumList.add(splitArrayTab[1].toString()+";"+splitArrayTab[2].toString());
-            	if(iDifficulty.equals("hard")) {
-            		String lati = splitArrayTab[3].toString();
-            		String longi =  splitArrayTab[4].toString();
-            		longi = longi.substring(0, longi.length()-2);
-            		hardList.add(lati + "," + longi + ";" + splitArrayTab[2].toString());
-            	}
-            }
-            
-        } catch (IOException e) {
+			readbuffer = new BufferedInputStream(stream);
+			bytesRead = 0;
+			String[] splitArrayLine = null;
+			String[] splitArrayTab = null;
 
-            e.printStackTrace();// handle
+			// Read File row-wise
+			while ((bytesRead = readbuffer.read(buffer)) != -1) {
+				String rawCity = new String(buffer, 0, bytesRead);
+				System.out.print(rawCity);
+				splitArrayLine = rawCity.split("\n");
+			}
 
-        }
-        finally {
-            if (stream != null) {
-                  try {
-                      readbuffer.close();
-                	  stream.close();
-                  } catch (IOException e) {}
-            }
-        }
+			System.out.println(""+splitArrayLine.length);
+			
+			// Add rows to corresponding ArrayList
+			for(int i = 0; i < splitArrayLine.length; i++) {
+				splitArrayTab = splitArrayLine[i].split("\t");
+
+				if(iDifficulty.equals("easy")) {
+					easyList.add(splitArrayTab[1].toString()+";"+splitArrayTab[2].toString());
+				}
+				if(iDifficulty.equals("medium")) {
+					mediumList.add(splitArrayTab[1].toString()+";"+splitArrayTab[2].toString());
+				}
+				if(iDifficulty.equals("hard")) {
+					String lati = splitArrayTab[3].toString();
+					String longi =  splitArrayTab[4].toString();
+					longi = longi.substring(0, longi.length()-2);
+					hardList.add(lati + "," + longi + ";" + splitArrayTab[2].toString());
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (stream != null) {
+				try {
+					readbuffer.close();
+					stream.close();
+				} catch (IOException e) {}
+			}
+		}
 	}
-	@Override
-	protected Object doInBackground(Object... arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 }
